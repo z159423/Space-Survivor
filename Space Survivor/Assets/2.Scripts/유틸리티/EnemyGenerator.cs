78b46aa741c972145986c002b4a93618a9d3d159
tero <dotnr3028@naver.com>
2022-11-11 01:48:53 +0900

    스폰웨이브 스크립트어블오브젝트화, 맵 배경 추가, 적 스탯 수정

--- Space Survivor/Assets/2.Scripts/유틸리티/EnemyGenerator.cs
+++ Space Survivor/Assets/2.Scripts/유틸리티/EnemyGenerator.cs
@@ -12,9 +12,12 @@ public class EnemyGenerator : MonoBehaviour
 
     public Transform parent;
 
+    [Space]
+
     //[ArrayElementTitle("enemyObject")]
     [SerializeField]
     public List<EnemyWave> enemySpawnWaves = new List<EnemyWave>();
+    [field: SerializeField] public SpawnWaveObject currentEnemySpawnWaveObject {get; private set;}
     [Space]
 
     //[ArrayElementTitle("enemyObject")]
@@ -79,16 +82,16 @@ public class EnemyGenerator : MonoBehaviour
 
     public void CheckWave()
     {
-        for (int i = 0; i < enemySpawnWaves.Count; i++)
+        for (int i = 0; i < currentEnemySpawnWaveObject.enemySpawnWaves.Count; i++)
         {
-            if (enemySpawnWaves[i].StartWaveTime == GameManager.instance.getCurrentTime())
+            if (currentEnemySpawnWaveObject.enemySpawnWaves[i].StartWaveTime == GameManager.instance.getCurrentTime())
             {
-                StartWave(enemySpawnWaves[i]);
+                StartWave(currentEnemySpawnWaveObject.enemySpawnWaves[i]);
             }
 
-            if (enemySpawnWaves[i].StopWaveTime == GameManager.instance.getCurrentTime())
+            if (currentEnemySpawnWaveObject.enemySpawnWaves[i].StopWaveTime == GameManager.instance.getCurrentTime())
             {
-                StopWave(enemySpawnWaves[i]);
+                StopWave(currentEnemySpawnWaveObject.enemySpawnWaves[i]);
             }
         }
     }
@@ -135,9 +138,9 @@ public class EnemyGenerator : MonoBehaviour
 
     public void StopAllWave()
     {
-        for (int i = 0; i < enemySpawnWaves.Count; i++)
+        for (int i = 0; i < currentEnemySpawnWaveObject.enemySpawnWaves.Count; i++)
         {
-            StopWave(enemySpawnWaves[i]);
+            StopWave(currentEnemySpawnWaveObject.enemySpawnWaves[i]);
         }
     }
 
@@ -342,111 +345,7 @@ public class EnemyGenerator : MonoBehaviour
     // }
 }
 
-[System.Serializable]
-public class EnemyWave
-{
 
-    [Range(0, 1800)]
-    public int StartWaveTime;
-    [Range(0, 1800)]
-    public int StopWaveTime;
-
-    [Space]
-    [SerializeField]
-    public waveType waveType;
-    [Space]
-    public float summonCycleTime = 1f;
-
-    public EnemyObject enemyObject;
-
-    public int count;
-
-    public Coroutine waveCoroutine;
-
-    public IEnumerator SummonPreiodically()
-    {
-        while (true)
-        {
-            if (EnemyGenerator.instance.spawningEnemy && !EnemyGenerator.instance.bossFighting)
-                EnemyGenerator.instance.GenerateEnemy2(enemyObject);
-
-            //MonoBehaviour.print(enemyObject + " / " + StartWaveTime + " / " + StopWaveTime);
-
-            yield return new WaitForSeconds(summonCycleTime);
-        }
-    }
-
-    public IEnumerator SummonBoss(GameObject panel, Animator animator)
-    {
-        panel.SetActive(true);
-        animator.SetTrigger("Active");
-        EnemyGenerator.instance.bossFighting = true;
-
-        AudioManager.instance.GenerateAudioAndPlaySFX("siren", Vector3.zero);
-
-        yield return new WaitForSeconds(3f);
-
-        panel.SetActive(false);
-
-        if (EnemyGenerator.instance.spawningEnemy)
-            EnemyGenerator.instance.GenerateEnemy2(enemyObject);
-
-        //MonoBehaviour.print(enemyObject + " / " + StartWaveTime + " / " + StopWaveTime);
-
-        /*while (true)
-        {
-            if (EnemyGenerator.instance.spawningEnemy)
-                EnemyGenerator.instance.GenerateEnemy2(enemyObject);
-
-            yield return new WaitForSeconds(summonCycleTime);
-        }*/
-    }
-
-    public IEnumerator blobBurstSummon()
-    {
-
-        if (EnemyGenerator.instance.spawningEnemy)
-            EnemyGenerator.instance.GenerateOneSpot(enemyObject, 50);
-
-        yield return null;
-    }
-
-    public IEnumerator hyperBurstSummon(GameObject panel, Animator animator)
-    {
-        panel.SetActive(true);
-        animator.SetTrigger("Active");
-
-        MonoBehaviour.print("11");
-
-        EnemyGenerator.instance.bossFighting = true;
-
-        AudioManager.instance.GenerateAudioAndPlaySFX("siren", Vector3.zero);
-
-        yield return new WaitForSeconds(3f);
-
-        MonoBehaviour.print("22");
-
-        panel.SetActive(false);
-
-        for (int i = 0; i < count; i++)
-        {
-            yield return null;
-            if (EnemyGenerator.instance.spawningEnemy)
-                EnemyGenerator.instance.GenerateEnemy2(enemyObject);
-        }
-
-        MonoBehaviour.print("33");
-
-        yield return new WaitForSeconds(10f);
-
-        MonoBehaviour.print("44");
-
-
-        EnemyGenerator.instance.bossFighting = false;
-    }
-
-    //public WaveObject waveObject;
-}
 
 /*[System.Serializable]
 public class EnemyPool
