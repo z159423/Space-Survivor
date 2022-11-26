91732dc0295b90c2221580116e9dbc2eb40ff61e
방승환 <tmdghks992@naver.com>
2022-11-27 03:17:40 +0900

    메디에이션 테스트 추가 완료

---
+++ Assets/2.Scripts/유틸리티/MediationTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAdsMediationTestSuite.Api;
+
+public class MediationTest : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+        }
+    }
+
+    public void show()
+    {
+        MediationTestSuite.Show();
+    }
+}
