1073d0f457f8ea3ccb8b08fa6380b01ce2458547
방승환 <tmdghks992@naver.com>
2022-12-17 01:40:51 +0900

    소용돌이 추가 출시

--- Assets/2.Scripts/투사체/Projectile_SpiralVortex.cs
+++ Assets/2.Scripts/투사체/Projectile_SpiralVortex.cs
@@ -71,7 +71,7 @@ public class Projectile_SpiralVortex : ProjectileLogic
             return;
 
         //적에게 들어가는 실제 데미지
-        int currentDamage = Mathf.RoundToInt((damage.GetFinalStatValueAsInt() + (playerWeapon.playerShipData.baseDamage.GetFinalStatValueAsInt()) * playerWeapon.additionalDamage.GetFinalStatValue()) / 3);
+        int currentDamage = Mathf.RoundToInt((damage.GetFinalStatValueAsInt() + (playerWeapon.playerShipData.baseDamage.GetFinalStatValueAsInt()) * playerWeapon.additionalDamage.GetFinalStatValue()) / 2);
 
         collision.GetComponent<EnemyStat>().TakeDamage(currentDamage);
         collision.GetComponent<EnemyStat>().Knockback(collision.bounds.center, knockBackForce);
