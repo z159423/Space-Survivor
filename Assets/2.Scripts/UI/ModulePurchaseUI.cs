211788ac286765aaaf19bb894f66af3101f769f1
방승환 <tmdghks992@naver.com>
2023-03-20 02:05:50 +0900

    ModulePurchaseUI

---
+++ Assets/2.Scripts/UI/ModulePurchaseUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ModulePurchaseUI : MonoBehaviour
+{
+    public void PurchaseModuleBox(int count)
+    {
+
+    }
+
+    public void CloseUI()
+    {
+        Destroy(gameObject);
+    }
+}
